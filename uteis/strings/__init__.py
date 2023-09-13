fa3756e326ea24743f4d950c53b9e1bead692997
Guilherme Simões Rosa <133579859+gurautnso@users.noreply.github.com>
2023-09-13 01:15:43 -0300

    Update strings.py

--- uteis/strings/__init__.py
+++ uteis/strings/__init__.py
@@ -1,6 +1,8 @@
 def titulo(prompt, tam=42):
+    if tam == 0:
+        tam = len(prompt) + 4
     print(f'{"="*tam}\n{prompt: ^{tam}}\n{"="*tam}')
 
 
-def linha(tam=42):
+def lin(tam=42):
     print('='*tam)
